320adeab46806d7ba5dcf14d1be5dbaf9e7a2663
AlexandreGross <gross.alexandre@hotmail.fr>
2019-05-22 15:13:23 +0200

    Merge origin/master
    Conflicts:
    	src/java/com/bank/servlet/HomeServlet.java

--- src/fr/solutec/dao/ConnectBd.java
+++ src/fr/solutec/dao/ConnectBd.java
@@ -5,10 +5,30 @@
  */
 package fr.solutec.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author ESIC
  */
 public class ConnectBd {
+    protected static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    protected static final String DB_NAME= "fatorfit";
+    protected static final String URL = "jdbc:mysql://localhost/"+DB_NAME;
+    protected static final String USER = "root";
+    protected static final String PASSWORD = "";
     
+    static {
+        try {
+            Class.forName(DRIVER_NAME).newInstance();
+        } catch (Exception e) {
+            System.out.println("Le pilote ne s'est pas chargé.");
+        }
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
