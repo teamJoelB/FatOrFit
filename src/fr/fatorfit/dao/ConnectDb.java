4f6e04a4b4fded2650248ac0f043c5c82f2e7fa5
AlexandreGross <gross.alexandre@hotmail.fr>
2019-05-22 16:40:22 +0200

    Merge origin/master
    Conflicts:
    	src/java/com/bank/servlet/HomeServlet.java

--- src/fr/fatorfit/dao/ConnectDb.java
+++ src/fr/fatorfit/dao/ConnectDb.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fr.solutec.dao;
+package fr.fatorfit.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
